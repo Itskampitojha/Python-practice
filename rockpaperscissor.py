d6c2074c725017b538b6f765d6dbe387ab35b75c
Kampit Ojha <97059622+Itskampitojha@users.noreply.github.com>
2023-10-25 17:50:35 +0530

    commit

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -2,14 +2,21 @@ import random
 
 user_wins = 0
 computer_wins = 0
+
+options = ["rock", "Paper", "Scissors", "test"]
+options[0]
+
 while True:
     user_input = input ("Type Rock/Paper/Scissors or Q to quit: ").lower ()
     if user_input == "q":
     break
 
-    if user_input not in ["rock", "Paper", "Scissors"]:
+    if user_input not in :
         continue
     
     random_number = random.randint (0,2)
+
+
+
 
     print("Goodbye!")
