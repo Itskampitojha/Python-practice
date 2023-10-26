d9a8aee638be8752d672e0895c3976c5c5eb41b8
Kampit Ojha <97059622+Itskampitojha@users.noreply.github.com>
2023-10-26 17:58:36 +0530

    breaking point

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -3,20 +3,23 @@ import random
 user_wins = 0
 computer_wins = 0
 
-options = ["rock", "Paper", "Scissors", "test"]
-options[0]
+options = ["rock", "paper", "scissors"]
 
 while True:
-    user_input = input ("Type Rock/Paper/Scissors or Q to quit: ").lower ()
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
-    break
+        break
 
-    if user_input not in :
+    if user_input not in options:
         continue
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+
     
-    random_number = random.randint (0,2)
-
-
-
-
-    print("Goodbye!")
