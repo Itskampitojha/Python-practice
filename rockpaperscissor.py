e8805e38f9125cb1da43b81ee7a1d755376cb0d6
Kampit Ojha <97059622+Itskampitojha@users.noreply.github.com>
2023-10-26 17:59:06 +0530

    Done

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -22,4 +22,18 @@ while True:
         print("You won!")
         user_wins += 1
 
-    
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
